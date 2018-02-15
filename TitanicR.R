4078f014b715fc888adfdcda1262ccd79a71c789
Carlos Coto <cmcoto@gmail.com>
2018-02-15 07:31:16 -0600

    Changed MICE to use ALL Data...

--- TitanicR.R
+++ TitanicR.R
@@ -18,10 +18,6 @@ summary(train)
 test$Survived <- NA
 all <- rbind(train, test)
 
-#factorize some variables
-
-fsurvived <- as.factor(train$Survived)
-fpclass <- as.factor(train$Pclass)
 
 #find NA in Variables, apparently in AGE we have 177, and appears to be the variable most affected...
 # What percent does the NA's account for the dataset? Is it 5% of Variable?
@@ -31,31 +27,31 @@ fpclass <- as.factor(train$Pclass)
 #https://datascienceplus.com/imputing-missing-data-with-r-mice-package/
 
 pMiss <- function(x){sum(is.na(x))/length(x)*100}
-apply(train,2,pMiss)
-apply(train,1,pMiss)
+apply(all,2,pMiss)
+apply(all,1,pMiss)
 
 #Use mice to find missing data with md function
-md.pattern(train)
+md.pattern(all)
 
 
 
 #Should I change the NA in Age with the MEAN of Age? Or should I check per case, if NAME or Title implies age?
 #I get the index of the NA
-naage <- which(is.na(train$Age))
+#naage <- which(is.na(train$Age))
 
 #get the names of naage to imply age?
-train$Name[naage]
+#train$Name[naage]
 
 #Master in name
-na_in_name <- which(grepl("Master", train$Name[naage]))
-na_in_name
+#na_in_name <- which(grepl("Master", train$Name[naage]))
+#na_in_name
 
 #USE DPLYR to select variables to be used with MICE
-dtrain <- select(train, Survived, Pclass, Age, SibSp, Parch, Fare)
-dtrain
+dall <- select(all, Survived, Pclass, Age, SibSp, Parch, Fare)
+dall
 
 #USE MICE WITH VARIABLES 
-imp <- mice(dtrain, meth='pmm',seed=500)
+imp <- mice(dall, meth='pmm',seed=500)
 summary(imp)
 
 #check imputed variables
@@ -63,10 +59,15 @@ imp$imp$Age
 #complete the Data
 imp <- complete(imp,1)
 
+#factorize some variables
+
+fsurvived <- as.factor(all$Survived)
+fpclass <- as.factor(all$Pclass)
+
  
 #factor Age according to Life Cycle Stages
-fage <- as.factor(train$Age) 
-cfage <- cut(train$Age, c(0,12, 20, 40, 60, Inf))
+fage <- as.factor(all$Age) 
+cfage <- cut(all$Age, c(0,12, 20, 40, 60, Inf))
 #factor fage levels
 #levels(fage) <- c("Childhood" = 0 <= 12, "Adolescense" = 13 <= 20, "Early Adult" = 21 <= 40, "Midlife" = 41 <= 60 "Mature" = 60 > 100)
 
@@ -76,9 +77,9 @@ cfimp_age <- cut(imp$Age, c(0,12, 20, 40, 60, Inf))
 
 #PLOTS
 
-plot(train$Sex, fsurvived, main = "Titanic Survival According To Gender", xlab = "Gender", ylab ="Survival")
+plot(all$Sex, fsurvived, main = "Titanic Survival According To Gender", xlab = "Gender", ylab ="Survival")
 
-plot(cfage, fsurvived, main = "Titanic Survival According To Life Cycle Stages", xlab = "Life Stage", ylab ="Survival")
+plot(cfimp_age, fsurvived, main = "Titanic Survival According To Life Cycle Stages", xlab = "Life Stage", ylab ="Survival")
 
 
 #MODELS
